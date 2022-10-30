9a245819bc7b010701e2406cb7eb5e4fa4f4845d
Aisorya B <aisorya.bhandari@gmail.com>
2022-10-30 14:05:54 -0400

    Add headshot and font type

--- src/components/MainPic.js
+++ src/components/MainPic.js
@@ -1,12 +1,16 @@
 import "./MainPicStyles.css"
 
 import React from 'react'
+import headshot from "../assets/headshot-img.svg"
 
 const MainPic = () => {
   return (
     <div className="Main">
         <div className = "mask">
-            
+          <img className="headshot" src={headshot} alt="headshot"/>
+        </div>
+        <div className="description">
+          <p>Hi, I'm a Computer Science Student</p>
         </div>
       
     </div>
