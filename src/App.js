e7b739a99e18e87469c3576e72f236c0bb0fda7d
sun971 <aisorya.bhandari@gmail.com>
2024-07-20 17:52:37 -0400

    Add blog section template starter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
-import Home from './routes/Home';
-import Journey from './routes/Journey';
-import Project from './routes/Project';
+import Home from "./routes/Home";
+import Journey from "./routes/Journey";
+import Project from "./routes/Project";
+import Blog from "./routes/Blog";
 
 function App() {
-  return (<>
-  <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/Journey" element={<Journey />}/>
-    <Route path="/Project" element={<Project />}/>
-
-  </Routes>
-  </>
-
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Journey" element={<Journey />} />
+        <Route path="/Project" element={<Project />} />
+        <Route path="/Blog" element={<Blog />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Journey/JourneyList.js
+++ src/components/Journey/JourneyList.js
@@ -1,6 +1,8 @@
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import "./JourneyList.css";
+
 import {
   ScrollContainer,
   ScrollPage,
@@ -35,13 +37,44 @@ const JourneyList = () => {
         </ScrollPage>
         <ScrollPage page={2}>
           <Animator animation={ZoomInScrollOut}>
-            <h2> Wealthsimple: </h2>
-            {/* I was previously an intern at Wealthsimple from April 2022 - December 2022 and during 2023 summer!  */}
+            <h2 className="page2-header"> Wealthsimple 💸 </h2>
+
+            <p className="page2-content">
+              I was previously an intern at Wealthsimple from April 2022 -
+              December 2022, also in the summer of 2023! In the summer of 2023,
+              I was on the Money Movement Team; the team responsible for
+              everything EFT (electronic funds transfer) related. Or in other
+              words, ensuring the movement of money in, out and in between
+              Wealthsimple accounts are fast and seamless. Prior to that, I was
+              on the Enablement Engineering Team; the team responsible for
+              implementing solutions and features for the client success team
+              and engineering teams.
+              <br />
+              <br />
+              Relevant Tech Stack: Ruby, React, React Native, GraphQL, DataDog
+              <br />
+              <br />
+              Projects: EFT migration, Funding Intent Checklist, HBP/ LLP
+              account types, Instant Auto-Deposits, Self Serve Know your Client
+            </p>
+
+            {/* Insert images of the stuff */}
           </Animator>
         </ScrollPage>
         <ScrollPage page={3}>
           <Animator animation={FadeUp}>
-            <h2> test scrool 3😀</h2>
+            <h2 className="page3-header"> RBC 🏦 </h2>
+            <p className="page3-content">
+              Interned on the Innovation team (Summer 2021) and the OMNI Team
+              (Winter 2022), for a total of 8 month. In the OMNI team, I worked
+              on online banking features and mainly worked on improving legacy
+              codebases & performed extensive performance testing to ensure
+              reliability . In the Innovtion team, worked closely with SRE to
+              develop a dashboard to model post incident learning’s.
+              <br />
+              <br />
+              Relevant Tech Stack: Java, K6, React, Angular, Springboot
+            </p>
           </Animator>
         </ScrollPage>
       </ScrollContainer>
